8cd9eec6c2909549508e5132702c07fa80825692
cybervasyan <esaydashev@baltbet.ru>
2026-06-14 01:26:38 +0300

    feat: add all http methods to orderService + gRPC clients

--- order/cmd/main.go
+++ order/cmd/main.go
@@ -14,18 +14,22 @@ import (
 
 	customMiddleware "github.com/cybervasyan/pdididy-project/order/internal/middleware"
 	orderv1 "github.com/cybervasyan/pdididy-project/shared/pkg/openapi/order/v1"
-	"github.com/google/uuid"
-
+	inventoryv1 "github.com/cybervasyan/pdididy-project/shared/pkg/proto/inventory/v1"
+	paymentv1 "github.com/cybervasyan/pdididy-project/shared/pkg/proto/payment/v1"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 const (
-	httpPort  = "8080"
-	urlParams = "city"
+	httpPort = "8080"
 	// Таймауты для http-сервера
-	readHeaderTimeout = 5 * time.Second
-	shutdownTimeout   = 10 * time.Second
+	readHeaderTimeout      = 5 * time.Second
+	shutdownTimeout        = 10 * time.Second
+	paymentServerAddress   = "localhost:50051"
+	inventoryServerAddress = "localhost:50052"
 )
 
 type OrderStorage struct {
@@ -35,7 +39,9 @@ type OrderStorage struct {
 
 type OrderHandler struct {
 	orderv1.UnimplementedHandler
-	storage *OrderStorage
+	paymentClient   paymentv1.PaymentServiceClient
+	inventoryClient inventoryv1.InventoryServiceClient
+	storage         *OrderStorage
 }
 
 func NewOrderStorage() *OrderStorage {
@@ -44,9 +50,11 @@ func NewOrderStorage() *OrderStorage {
 	}
 }
 
-func NewOrderHandler(storage *OrderStorage) *OrderHandler {
+func NewOrderHandler(storage *OrderStorage, pay paymentv1.PaymentServiceClient, inv inventoryv1.InventoryServiceClient) *OrderHandler {
 	return &OrderHandler{
-		storage: storage,
+		storage:         storage,
+		paymentClient:   pay,
+		inventoryClient: inv,
 	}
 }
 
@@ -65,10 +73,170 @@ func (o *OrderHandler) GetOrderByUuid(ctx context.Context, params orderv1.GetOrd
 	return order, nil
 }
 
+func (o *OrderHandler) CreateOrder(ctx context.Context, req *orderv1.CreateOrderRequest) (r orderv1.CreateOrderRes, _ error) {
+	o.storage.mu.Lock()
+	defer o.storage.mu.Unlock()
+
+	orderToCreate := orderv1.OrderDto{
+		OrderUUID: uuid.New(),
+		UserUUID:  req.UserUUID,
+		PartUuids: req.PartUuids,
+		Status:    orderv1.OrderStatusPENDINGPAYMENT,
+	}
+
+	o.storage.orders[orderToCreate.OrderUUID] = &orderToCreate
+
+	return &orderv1.CreateOrderResponse{
+		OrderUUID: orderToCreate.GetOrderUUID(),
+	}, nil
+}
+
+func (o *OrderHandler) CancelOrder(ctx context.Context, params orderv1.CancelOrderParams) (r orderv1.CancelOrderRes, _ error) {
+	o.storage.mu.Lock()
+	defer o.storage.mu.Unlock()
+
+	order, ok := o.storage.orders[params.OrderUUID]
+	if !ok {
+		return &orderv1.NotFoundError{
+			Code:    404,
+			Message: "Такого элемента нет",
+		}, nil
+	}
+
+	if order.GetStatus() != orderv1.OrderStatusPENDINGPAYMENT {
+		return &orderv1.ConflictError{
+			Code:    422,
+			Message: "Заказ находится в статусе отличном от PENDING",
+		}, nil
+	}
+
+	order.SetStatus(orderv1.OrderStatusCANCELLED)
+	return &orderv1.CancelOrderNoContent{}, nil
+}
+
+func (o *OrderHandler) PayOrder(ctx context.Context, req *orderv1.PayOrderRequest, params orderv1.PayOrderParams) (r orderv1.PayOrderRes, _ error) {
+	o.storage.mu.Lock()
+
+	order, ok := o.storage.orders[params.OrderUUID]
+
+	if !ok {
+		o.storage.mu.Unlock()
+		return &orderv1.NotFoundError{
+			Code:    404,
+			Message: "Такого элемента нет",
+		}, nil
+	}
+
+	if order.GetStatus() != orderv1.OrderStatusPENDINGPAYMENT {
+		o.storage.mu.Unlock()
+		return &orderv1.ConflictError{
+			Code:    422,
+			Message: "Заказ находится в статусе отличном от PENDING",
+		}, nil
+	}
+
+	var partsStringUUIDs []string
+
+	for _, part := range order.PartUuids {
+		partsStringUUIDs = append(partsStringUUIDs, part.String())
+	}
+
+	o.storage.mu.Unlock()
+
+	parts, err := o.inventoryClient.ListParts(ctx, &inventoryv1.ListPartsRequest{Filter: &inventoryv1.PartsFilter{Uuids: partsStringUUIDs}})
+
+	if err != nil {
+		return &orderv1.InternalServerError{
+			Code:    500,
+			Message: "InventoryService не отвечает",
+		}, nil
+	}
+
+	var totalPrice float64
+
+	for _, part := range parts.Parts {
+		totalPrice += part.Price
+	}
+
+	var paymentMethod paymentv1.PaymentMethod
+
+	switch req.PaymentMethod {
+	case orderv1.PaymentMethodCARD:
+		paymentMethod = paymentv1.PaymentMethod_PAYMENT_METHOD_CARD
+	case orderv1.PaymentMethodSBP:
+		paymentMethod = paymentv1.PaymentMethod_PAYMENT_METHOD_SBP
+	case orderv1.PaymentMethodCREDITCARD:
+		paymentMethod = paymentv1.PaymentMethod_PAYMENT_METHOD_CREDIT_CARD
+	case orderv1.PaymentMethodINVESTORMONEY:
+		paymentMethod = paymentv1.PaymentMethod_PAYMENT_METHOD_INVESTOR_MONEY
+	case orderv1.PaymentMethodUNKNOWN:
+		paymentMethod = paymentv1.PaymentMethod_PAYMENT_METHOD_UNSPECIFIED
+	default:
+		paymentMethod = paymentv1.PaymentMethod_PAYMENT_METHOD_UNSPECIFIED
+	}
+
+	transactionUUID, err := o.paymentClient.PayOrder(ctx, &paymentv1.PayOrderRequest{
+		OrderUuid:     params.OrderUUID.String(),
+		UserUuid:      order.UserUUID.String(),
+		PaymentMethod: paymentMethod,
+	})
+
+	if err != nil {
+		return &orderv1.InternalServerError{
+			Code:    500,
+			Message: "PaymentService не отвечает",
+		}, nil
+	}
+	parsedUUID, err := uuid.Parse(transactionUUID.GetTransactionUuid())
+
+	if err != nil {
+		return &orderv1.InternalServerError{
+			Code:    500,
+			Message: "PaymentService сломался",
+		}, nil
+	}
+
+	o.storage.mu.Lock()
+	order.SetStatus(orderv1.OrderStatusPAID)
+	order.SetTotalPrice(totalPrice)
+	order.SetPaymentMethod(orderv1.NewOptPaymentMethod(req.PaymentMethod))
+	order.SetTransactionUUID(orderv1.NewOptNilUUID(parsedUUID))
+	o.storage.mu.Unlock()
+	return &orderv1.PayOrderResponse{
+		TransactionUUID: parsedUUID,
+	}, nil
+}
+
 func main() {
 	storage := NewOrderStorage()
+	pay, err := grpc.NewClient(paymentServerAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-	orderHandler := NewOrderHandler(storage)
+	if err != nil {
+		log.Fatalf("Ошибка создания gRPC сервера PaymentService: %v", err)
+	}
+
+	inv, err := grpc.NewClient(inventoryServerAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Ошибка создания gRPC сервера InventoryService: %v", err)
+	}
+
+	defer func() {
+		if cerr := pay.Close(); cerr != nil {
+			log.Printf("failed to close connect: %v", cerr)
+		}
+	}()
+
+	defer func() {
+		if cerr := inv.Close(); cerr != nil {
+			log.Printf("failed to close connect: %v", cerr)
+		}
+	}()
+
+	payClient := paymentv1.NewPaymentServiceClient(pay)
+	invClient := inventoryv1.NewInventoryServiceClient(inv)
+
+	orderHandler := NewOrderHandler(storage, payClient, invClient)
 
 	orderServer, err := orderv1.NewServer(orderHandler)
 	if err != nil {
--- scripts/test-api.ps1
+++ scripts/test-api.ps1
@@ -1,4 +1,4 @@
-param(
+﻿param(
     [string]$GrpcurlPath = ".\bin\grpcurl.exe"
 )
 
